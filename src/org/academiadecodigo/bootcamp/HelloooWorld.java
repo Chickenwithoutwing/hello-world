860ccb34f60a90367e2c3c3f9d6c7f63f0109d55
Franga <bernardo.d.c.g.telo@gmail.com>
2019-09-18 17:25:11 +0100

    <one more line>

---
+++ src/org/academiadecodigo/bootcamp/HelloooWorld.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.bootcamp;
+
+public class HelloooWorld {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello World Fuckers!!");
+    }
+}
